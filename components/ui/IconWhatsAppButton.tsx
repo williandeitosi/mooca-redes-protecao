96305d9179fffa29253a8929438a223109684a8f
williandeitosi <williandeitosi@gmail.com>
2025-01-21 20:52:27 -0300

    fix: bug router

--- components/ui/IconWhatsAppButton.tsx
+++ components/ui/IconWhatsAppButton.tsx
@@ -3,22 +3,32 @@
 import { handleWhatsAppClick } from "@/utils/handleWhatsAppClick";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function FloatingWhatsappIcon() {
+  const router = useRouter();
+
+  const handleClick = async (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    handleWhatsAppClick("Contato via WhatsApp (ícone flutuante)");
+
+    window.open(
+      `https://wa.me/5511963403041?text=${encodeURIComponent(
+        "Encontrei você pelo site e estou entrando em contato para solicitar uma cotação"
+      )}`,
+      "_blank"
+    );
+
+    router.push("/enviar_mensagem");
+  };
+
   return (
     <div
       className="fixed bottom-4 right-4 z-50 cursor-pointer hover:opacity-90 hover:scale-110 transform transition duration-150"
-      style={{ zIndex: 9999 }} // Garante que o ícone fique acima de outros elementos
+      style={{ zIndex: 9999 }}
     >
-      <Link
-        href={`https://wa.me/5511963403041?text=${encodeURIComponent(
-          "Encontrei você pelo site e estou entrando em contato para solicitar uma cotação"
-        )}`}
-        target="_blank"
-        onClick={() =>
-          handleWhatsAppClick("Contato via WhatsApp (ícone flutuante)")
-        }
-      >
+      <Link href="/enviar_mensagem" onClick={handleClick}>
         <Image
           src="/images/whatsapp.png"
           alt="Fale conosco pelo WhatsApp"
--- components/ui/WhatsappButton.tsx
+++ components/ui/WhatsappButton.tsx
@@ -2,14 +2,33 @@
 
 import { Button } from "@/components/ui/button";
 import { handleWhatsAppClick } from "@/utils/handleWhatsAppClick";
+import { useRouter } from "next/navigation";
+
 interface WhatsappButtonProps {
   label: string;
 }
 
 export default function WhatsappButton({ label }: WhatsappButtonProps) {
+  const router = useRouter();
+
+  const handleClick = async (e: React.MouseEvent) => {
+    handleWhatsAppClick(label);
+
+    window.open(
+      `https://wa.me/5511963403041?text=${encodeURIComponent(
+        "Encontrei você pelo site e estou entrando em contato para solicitar uma cotação"
+      )}`,
+      "_blank"
+    );
+
+    router.push("/enviar_mensagem");
+
+    e.preventDefault();
+  };
+
   return (
     <Button
-      onClick={() => handleWhatsAppClick(label)}
+      onClick={handleClick}
       size="lg"
       className="mb-6 bg-gradient-to-r from-green-400 to-green-500 text-white px-6 py-3 rounded-lg transform transition duration-300 hover:scale-105 shadow-[0_0_10px_2px_rgba(34,197,94,0.7)] hover:shadow-[0_0_20px_5px_rgba(34,197,94,0.8)]"
     >
